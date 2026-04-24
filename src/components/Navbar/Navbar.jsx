84ad32395ed1dad4c442edb211b0d1172777d6ca
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-24 18:19:11 +0600

    user logout system done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,21 @@
-
+'use client'
+import { authClient, signOut } from '@/lib/auth-client';
+import { Button } from '@heroui/react';
 import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
+    const {
+        data: session,
+    } = authClient.useSession()
+
+    console.log(session)
+    const user = session?.user
+
+    const signOut = () => {
+
+        authClient.signOut()
+    }
     return (
         <div className='flex justify-between w-11/12 p-5 mx-auto'>
             <div>
@@ -26,13 +39,20 @@ const Navbar = () => {
                     </Link>
                 </ul>
             </div>
-            <div className='flex gap-5'>
+            <div className='flex items-center gap-5'>
                 <Link href={'/signup'}>
                     Sign Up
                 </Link>
                 <Link href={'/signin'}>
                     Sign In
                 </Link>
+                <div>
+                    {
+                        user?.name
+                    }
+                </div>
+                <Button variant='danger' onClick={signOut} >Logout</Button>
+
             </div>
         </div>
     );
